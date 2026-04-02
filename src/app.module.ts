08e62eab10734781490c29e5d9e817f0cbed0768
Ruhan Freitas <jspruhanfreitas@gmail.com>
2026-04-02 15:13:59 -0300

    feat: configure docker-compose with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
     imports: [
         AuthModule,
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            database: process.env.DATABASE,
-            synchronize: process.env.SYNCHRONIZE === '1',
-            autoLoadEntities: process.env.AUTO_LOAD_ENTITIES === '1'
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.get<string>('POSTGRES_HOST'),
+                port: config.get<number>('POSTGRES_PORT'),
+                username: config.get<string>('POSTGRES_USER'),
+                password: config.get<string>('POSTGRES_PASSWORD'),
+                database: config.get<string>('POSTGRES_DB'),
+                synchronize: config.get<string>('SYNCHRONIZE') === '1',
+                autoLoadEntities:
+                    config.get<string>('AUTO_LOAD_ENTITIES') === '1',
+                logging: true
+            })
         })
     ]
 })
